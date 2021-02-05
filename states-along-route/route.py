4d65d8b634ecf920a37b24ce01ef233223d43a00
marcm97 <marc.mascarenhas20@gmail.com>
2021-02-05 13:49:30 -0800

    initial file structure setup

---
+++ states-along-route/route.py
@@ -0,0 +1,26 @@
+
+
+
+class StatesAlongRoute:
+    def __init__(self,api_key,start,end):
+        self.api_key = api_key
+        self.start = start
+        self.end = end
+
+    """
+    returns a list of lat,longs for each turn
+    """
+    def get_route():
+        pass
+
+    """
+    returns a list of states that were passed through
+    """
+    def get_states():
+        pass
+
+    """
+    returns a dictionary of the distances travelled in each state
+    """
+    def get_distances():
+        pass
